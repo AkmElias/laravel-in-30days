41657c748e6abb70581f5d5a36c24483cead5554
AKM Elias <akmelias11@gmail.com>
2024-05-27 11:04:50 +0600

    day 3: layout and components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
-
 <html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Contact Page</title>
+    <title>Home Page</title>
 </head>
 <body>
-    <h1>Hello from contact page.</h1>
+    <nav> 
+        <x-nav-link>Home</x-nav-link>
+        <x-nav-link>About</x-nav-link>
+        <x-nav-link>Contact</x-nav-link>
+    </nav>
+
+    {{ $slot }}
 </body>
 </html>
---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,3 @@
+<a href="{{strtolower($slot)}}">
+    {{ $slot }}
+</a>
