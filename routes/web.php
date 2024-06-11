461433a2d65d41580b5ea3538690a39c7a002dc5
AKM Elias <akmelias11@gmail.com>
2024-06-11 20:50:06 +0600

    day18: updating, deleting

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+// index
 Route::get('jobs/', function (){
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
@@ -15,10 +16,12 @@ Route::get('jobs/', function (){
     ]);
 });
 
+// create 
 Route::get('jobs/create', function () {
     return view('jobs.create');
 });
 
+// show
 Route::get('jobs/{id}', function ($id) {
     // $job = Arr::first($jobs, fn ($job) => $job['id'] == $id); another way to get the job
     $job = Job::find($id);
@@ -28,6 +31,7 @@ Route::get('jobs/{id}', function ($id) {
     ]);
 });
 
+// store
 Route::post('jobs/', function () {
     //validation
     request()->validate([
@@ -47,6 +51,55 @@ Route::post('jobs/', function () {
     // $job->employer_id = 1;
     // $job->save();
 
+    return redirect('jobs/');
+});
+
+// edit
+Route::get('jobs/{id}/edit', function ($id) {
+    // $job = Arr::first($jobs, fn ($job) => $job['id'] == $id); another way to get the job
+    $job = Job::find($id);
+
+    return view('jobs.edit', [
+        'job' => $job,
+    ]);
+});
+
+// update
+Route::patch('jobs/{id}', function ($id) {
+    //validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
+    // authorize
+    // $job = Job::find($id);
+    // if ($job->employer_id !== 1) {
+    //     abort(403);
+    // }
+
+    // update the job
+    $job = Job::findOrFail($id);
+
+    // $job->title = request('title');
+    // $job->salary = request('salary');
+    // $job->save();
+
+    // another option
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+       ]);
+
+    // redirect
+    return redirect('jobs/' . $id);
+});
+
+// destroy
+Route::delete('jobs/{id}', function ($id) {
+    // authorize (on hold...)
+    Job::findOrFail($id)->delete();
+    // redirect
     return redirect('jobs/');
 });
 
