941e8523626ba7529e79a998f16fd13805829eb5
AKM Elias <akmelias11@gmail.com>
2024-06-19 21:29:44 +0600

    day26: asset bundling

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,7 +2,7 @@
     <x-slot:heading>Job</x-slot:heading>
     
     <h2 class="font-bold text-lg">{{$job->title}}</h2>
-    <p>This job pays {{$job->salary}} per year.</p>
+    <p class="text-laracasts">This job pays {{$job->salary}} per year.</p>
 
     @can('edit', $job)
         <p class="mt-6">
